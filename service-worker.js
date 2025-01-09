0899fb1643a6b8bbb12d157fdb812600f05ae7f9
ALion Alex <alionalexxx@gamil.com>
2025-01-09 15:46:47 +0200

    Updates

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) {
+        return clientList[0].focus();
+      }
+      return clients.openWindow('/pomo-timer/');
+    })
+  );
+});
